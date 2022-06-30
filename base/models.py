8ce233c910aa08eb1f84fc58e266edc3d1ec751b
Touyu Pan <firward84081441@gmail.com>
2022-06-30 23:38:26 +0800

    create model Room

--- base/models.py
+++ base/models.py
@@ -1,4 +1,17 @@
+from pydoc import describe
+from venv import create
 from django.db import models
 
 # Create your models here.
 
+class Room(models.Model):
+    #host = 
+    #topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True) # auto_now takes a snapshot on every time we save
+    created = models.DateTimeField(auto_now_add=True) # auto_now_add takes a time stamp when we first create this instance
+
+    def __str__(self):
+        return self.name
